c6e4fd4f7f31a5195154ee915afdf1f08054577a
Jakk_Kor-u667906l00211 <jakkrid.momkratoke@seagate.com>
2024-06-14 14:45:10 +0700

    add Paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-dark vh-100 overflow-auto p-3">
-      <div className="container text-center p-3 bg-light" style={{Height:"400px"}}>
+      <div className="container text-center p-3 bg-light" style={{ Height: "400px" }}>
         <h1 className="text-success bg-dark">
-          เวลคัม เวลคัม แฮฟรูม แฮฟดอนด้อม กู้ดเทคแคร์ ดูเอฟวรี่ติง
+          เวลคัม
         </h1>
+        <p>
+          ฮัลโลวว เว้ลค้าม แฮฟรูมแฮฟคอนด้อมแฮฟเควาย กู้ดเทคแคร์ ดูเอวี่ติง ฟัคแอส ฟัคพุ๊ซซี่ ซัคยัวร์ดิก ลิคยัวร์บอล ลิคยัวร์แอส ซัคลองเวกู๊ดจ๊อบโบลว จ๊อบแฮนด์จ๊อบฟุตจ๊อบ ฟัคมายแอสคัมมิงมายเม้า ฟัคมายเม้าคัมมิงมายแอส ฟัคมายแอสคัมมิงมายเฟค ลุคกิ้งมายอาย แต้งกิ้ว เวลคัม ม๊วบบบบ!!
+        </p>
       </div>
     </main>
   );
