f15c3b69f39881d00b4c01f3870053f72bd0d605
Jakk_Kor-u667906l00211 <jakkrid.momkratoke@seagate.com>
2024-06-14 14:46:41 +0700

    add Paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-dark vh-100 overflow-auto p-3">
-      <div className="container text-center p-3 bg-light" style={{ Height: "400px" }}>
+      <div className="container text-center p-3 bg-light">
         <h1 className="text-success bg-dark">
           เวลคัม
         </h1>
