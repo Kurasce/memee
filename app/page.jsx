bca8db73d3ce1d00da4d73e354a377f5d8287834
Jakk_Kor-u667906l00211 <jakkrid.momkratoke@seagate.com>
2024-06-14 14:36:45 +0700

    change mainpage H1

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-dark vh-100 overflow-auto p-3">
+      <div className="container text-center p-3 bg-light" style={{Height:"400px"}}>
+        <h1 className="text-success bg-dark">
+          เวลคัม เวลคัม แฮฟรูม แฮฟดอนด้อม กู้ดเทคแคร์ ดูเอฟวรี่ติง
+        </h1>
+      </div>
+    </main>
+  );
+}
